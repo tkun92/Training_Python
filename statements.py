e5d1592da24c3a1af86b57f63f1394df2d17b512
tkun92 <kun.t1992@gmail.com>
2022-03-02 15:34:36 +0100

    délután

---
+++ statements.py
@@ -0,0 +1,20 @@
+print(5*6)
+print(5+6)
+print(3/2)
+print(3-2)
+print(5/6+12)
+print(1+2*3)
+
+print("alma" + "körte")
+print("alma" + str(5))
+print(str(3) + "alma")
+print("alma".replace("ma",""))
+#print("alma" * "körte")
+print("alma" * 3)
+
+hours = 4
+minutes = hours*60
+print(str(hours) + "óra az ennyi perc:" + str(minutes))
+
+word= "aztamindenségitneki"
+print(len(word))
---
+++ user_interface.py
@@ -0,0 +1,5 @@
+name = input("add meg a neved")
+print(name)
+
+number = input("adj meg egy számot és én megszorzom öttel")
+print(number*5)
