504f53784985144f0ef0d3b811227eb6675889c2
tkun92 <kun.t1992@gmail.com>
2022-03-02 16:18:26 +0100

    for

--- control_loop.py
+++ control_loop.py
@@ -1,2 +1,8 @@
 for i in range(0,5):
     print("hello world")
+
+name = input("add meg a nevedet")
+repeat = int(input("mennyiszer szeretnéd látni a nevedet"))
+
+for i in range(repeat):
+    print(str(i+1) + ". " + name)
